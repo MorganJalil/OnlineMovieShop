413bc24186ba199e0fee71edb949793b854b7107
Morgan J <Moggelicious@hotmail.com>
2019-04-22 12:00:39 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MovieLibraryComponent } from './components/movie-library/movie-library.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieLibraryComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/movie-library/movie-library.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MovieLibraryComponent } from './movie-library.component';
+
+describe('MovieLibraryComponent', () => {
+  let component: MovieLibraryComponent;
+  let fixture: ComponentFixture<MovieLibraryComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MovieLibraryComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MovieLibraryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
